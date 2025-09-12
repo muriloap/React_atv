e5246cfb1081ecfeca245c02f1a6f41d001f65b6
muriloap <muprado222@gmail.com>
2025-09-12 15:28:24 -0300

    usando o useState

--- src/app/contato/page.tsx
+++ src/app/contato/page.tsx
@@ -1,18 +1,25 @@
+'use client';
 import Textfield from '@/components/Textfield'
 import styles from './styles.module.css'
 
 
 export default function Contato() {
+    function botaoOnClick() {
+        alert("CLICK");
+    }
+
     return (
         <>
-            <h1>Contato</h1>
+        <div className={styles.div}>
+            <h1 className={styles.h1}>Contato</h1>
             <form action="">
                <Textfield label='Nome' type='text'/>
                <Textfield label='E-mail' type='email'/>
                <Textfield label='Assunto' type='text'/>
                <Textfield label='Mensagem' type='text' multiline />
-                <button>Enivar</button>
+                <button onClick={botaoOnClick} className={styles.button}>Enivar</button>
             </form>
+        </div>
         </>
     )
 }
--- src/components/Textfield/index.tsx
+++ src/components/Textfield/index.tsx
@@ -1,22 +1,33 @@
+'use client';
+import { ChangeEvent, useState } from 'react';
 import styles from './styles.module.css'
 
 type Props = {
     label: string;
     type: "text" | "email";
     multiline?: boolean;
+    onChange?(): void;
 }
 
 export default function Textfield(props: Props) {
+
+    const [texto, setTexto] = useState("");
+    
+    function handleChange(e: ChangeEvent<HTMLInputElement>) {
+        setTexto(e.target.value)
+    }
+
     return (
-        <>
+        <div className={styles.root}>
         <label>
         {props.label}: 
         {(props.multiline) ? (
             <textarea/>
         ) : (
-            <input type={props.type} placeholder='Nome' />
+            <input onChange={handleChange} type={props.type} placeholder={props.label} />
         )}
+        {texto}
         </label>
-        </>
+        </div>
     );
 }
